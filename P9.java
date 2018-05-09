2878b9dbf4d7ee615a5c1dd185558aed5906cd57
Coaja Andreea <a.coaja@yahoo.ro>
2018-05-09 16:27:47 +0300

    modificat

--- P9.java
+++ P9.java
@@ -1,3 +1,5 @@
+import static java.lang.Math.*;
+
 public class P9 {
     public static void main(String[] args) {
 
@@ -5,22 +7,11 @@ public class P9 {
         double vitezaIon=SkeletonJava.readDoubleConsole("Viteza lui Ion este: ");
         double vitezaFlorica=SkeletonJava.readDoubleConsole("Viteza Floricai este: ");
         double distantaIon=0;
-        double distantaFlorica=0;
         double timp=0;
-        double delta=0;
 
-        delta=Math.sqrt(distantaDintreOrase*distantaDintreOrase-4*vitezaFlorica*vitezaIon);
+        distantaIon=distantaDintreOrase*vitezaIon/(vitezaFlorica+vitezaIon);
 
-        if(distantaDintreOrase>delta)
-        {
-            distantaIon=(distantaDintreOrase+delta)/-2;
-        }
-        else
-        {
-            distantaIon=(distantaDintreOrase-delta)/-2;
-        }
-
-        timp=vitezaIon/distantaIon*60;
+        timp=distantaIon/vitezaIon*60;
 
         System.out.println("Ion si Florica se intalnesc dupa "+timp+" minute");
         System.out.println("Distanta parcursa de Ion este de "+distantaIon+"km");
